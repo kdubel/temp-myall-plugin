bd446e9ea4a56288ef7cf04cc5107f75af66bfed
Tomasz Maka <tomasz.maka@themasters.io>
2017-09-06 14:24:58 +0200

    init version

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-header
+# about: Header links
+# version: 0.0.1
+# authors: themasters.io
+
+register_asset "stylesheets/desktop/header.scss", :desktop
+register_asset "stylesheets/mobile/header.scss", :mobile
